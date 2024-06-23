26976035b4b708880e7a4848d246184ca03e7a71
Sergey <1@php-cat.com>
2024-06-24 00:20:01 +0500

    38

--- app/Livewire/Upr/Cron/Action/SaveFileIsArray.php
+++ app/Livewire/Upr/Cron/Action/SaveFileIsArray.php
@@ -14,6 +14,69 @@ class SaveFileIsArray extends Component
     public $result3 = '';
 
 
+// Функция для преобразования прав доступа в формат chmod
+    function getChmod($file)
+    {
+        $perms = fileperms($file);
+
+        if (($perms & 0xC000) == 0xC000) {
+            // Сокет
+            $info = 's';
+        } elseif (($perms & 0xA000) == 0xA000) {
+            // Символическая ссылка
+            $info = 'l';
+        } elseif (($perms & 0x8000) == 0x8000) {
+            // Обычный файл
+            $info = '-';
+        } elseif (($perms & 0x6000) == 0x6000) {
+            // Специальный блок устройства
+            $info = 'b';
+        } elseif (($perms & 0x4000) == 0x4000) {
+            // Каталог
+            $info = 'd';
+        } elseif (($perms & 0x2000) == 0x2000) {
+            // Специальный символьный файл
+            $info = 'c';
+        } elseif (($perms & 0x1000) == 0x1000) {
+            // Сокет FIFO
+            $info = 'p';
+        } else {
+            // Неизвестный
+            $info = 'u';
+        }
+
+        // Владелец
+        $info .= (($perms & 0x0100) ? 'r' : '-');
+        $info .= (($perms & 0x0080) ? 'w' : '-');
+        $info .= (($perms & 0x0040) ?
+            (($perms & 0x0800) ? 's' : 'x') :
+            (($perms & 0x0800) ? 'S' : '-'));
+
+        // Группа
+        $info .= (($perms & 0x0020) ? 'r' : '-');
+        $info .= (($perms & 0x0010) ? 'w' : '-');
+        $info .= (($perms & 0x0008) ?
+            (($perms & 0x0400) ? 's' : 'x') :
+            (($perms & 0x0400) ? 'S' : '-'));
+
+        // Мир
+        $info .= (($perms & 0x0004) ? 'r' : '-');
+        $info .= (($perms & 0x0002) ? 'w' : '-');
+        $info .= (($perms & 0x0001) ?
+            (($perms & 0x0200) ? 't' : 'x') :
+            (($perms & 0x0200) ? 'T' : '-'));
+
+        return $info;
+    }
+
+//// Путь к вашему файлу
+//$file = 'path/to/your/file.txt';
+//
+//// Получение и вывод прав доступа
+//$chmod = getChmod($file);
+//echo "Права доступа к файлу $file: $chmod";
+
+
     public function copyFileInContainer($container_name, $source_path, $target_path): array
     {
 //        $container_name = 'cron-service';
@@ -209,6 +272,11 @@ class SaveFileIsArray extends Component
         }
 
         ob_start();
+
+//// Получение и вывод прав доступа
+$chmod = getChmod($filename);
+echo "Права доступа к файлу $filename: $chmod";
+
 
 // Изменение прав доступа к файлу
         if (chmod($filename, $permissions)) {
