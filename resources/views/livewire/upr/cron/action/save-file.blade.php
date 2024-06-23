b4bb81873a42c7ff005434081baa3a2030a9245b
Sergey <1@php-cat.com>
2024-06-24 00:22:40 +0500

    39

--- resources/views/livewire/upr/cron/action/save-file.blade.php
+++ resources/views/livewire/upr/cron/action/save-file.blade.php
@@ -1,18 +1,31 @@
 <div>
-11<br/>
-    <form wire:submit="save" >
-    <textarea
-        wire:model="data_cron_file"
-        rows="10" style="width:100%;"></textarea>
-        <br/>
-        <button type="submit" class="p-1 bg-green-400" >Save</button>
-        <br/>
+
+{{--    --}}
+{{--11<br/>--}}
+{{--    <form wire:submit="save" >--}}
+{{--    <textarea--}}
+{{--        wire:model="data_cron_file"--}}
+{{--        rows="10" style="width:100%;"></textarea>--}}
+{{--        <br/>--}}
+{{--        <button type="submit" class="p-1 bg-green-400" >Save</button>--}}
+{{--        <br/>--}}
 {{--    <button--}}
 {{--        class="bg-yellow-100 hover:bg-yellow-200 p-1 text-gray-400 hover:text-black mr-2"--}}
 {{--        wire:click="saveCronFile">сохранить файл</button>--}}
-    </form>
+{{--    </form>--}}
+{{--    <br/>--}}
+{{--    22--}}
+
+
+    result: {{ $result ?? 'x' }}
     <br/>
-    22
+    result1: {{ $result1 ?? 'x' }}
+    <br/>
+    result2: {{ $result2 ?? 'x' }}
+    <br/>
+    result3: {{ $result3 ?? 'x' }}
+    <br/>
+
     <br/>
     <form wire:submit="save2" >
     <textarea
